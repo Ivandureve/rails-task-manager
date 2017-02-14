f9d55921ca828413adb6291508d206fd2aa3e181
Ivan Rimbaud <ivan.rimbaud@gmail.com>
2017-02-14 16:10:43 -0200

    week 6, day 2, ex 1

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,16 @@
 module TasksHelper
+
+  def tick(done)
+    if done
+      "✓"
+    else
+     "✕"
+   end
+  end
+
+  def prety_task(task)
+    if task.description
+      " - "
+    end
+  end
 end
